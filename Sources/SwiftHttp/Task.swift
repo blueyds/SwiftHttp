d183df7807d19e23a09c027759570ded6363d7c6
Blueyds <9055449+blueyds@users.noreply.github.com>
2023-09-02 09:49:43 -0500

    misc
    changed access level to internal on URLComponent extensions
    Added extension to cleaen up code adding an array of queries

--- Sources/SwiftHttp/Task.swift
+++ Sources/SwiftHttp/Task.swift
@@ -2,21 +2,23 @@ import Foundation
 
 public protocol HttpTask{
 	associatedtype T:Codable
-	var api: HttpUrlRequestor { get }
-	var url: URLComponents { get }
-	var headers: [String:String] { get }
+	var defaults: HttpEnvironment { get }
 	var method: HttpMethod { get }
+	var headers: [(name: String, value: String)] { get }
+	var path: String { get }
+	var queries: [(name: String, value: String)]
+	
 }
 
 extension HttpTask{
     public func run() async -> T {
         let session = URLSession.shared
-        let request = api.urlRequest(from: self)
+        let request = defaults.urlRequest(from: self)
         do {
             let result = try await session.download(for: request)
             print("got download \(result.0)")
-            let text = try String(contentsOf: result.0)
-            print(text)
+            // let text = try String(contentsOf: result.0)
+            // print(text)
             let data = try Data(contentsOf: result.0)
             return decode(from: data)
         }
