ae0a17ffce080e3b7bb53f3c2924d9835722e146
Blueyds <9055449+blueyds@users.noreply.github.com>
2023-09-02 09:55:53 -0500

    refactored
    refactored as HttpEnvironment
    changed items names
    converted dictionary to an array for simplicity

---
+++ Sources/SwiftHttp/HttpEnvironment.swift
@@ -0,0 +1,42 @@
+import Foundation
+
+
+
+public protocol HttpEnvironment{
+	var hostName: String { get }
+	var pathPrefix: String { get }
+	var headers: [(name: String, value: String)] { get }
+	var queries: [(name: String, value: String)] { get }
+	//func urlRequest(from task: any HttpTask) -> URLRequest
+}
+
+extension HttpEnvironment {
+	public func urlRequest(from task: any HttpTask) -> URLRequest{
+		var urlParts = URL()
+		urlParts.scheme = "https"
+		urlParts.host = hostName
+		urlParts.path = pathPrefix + task.path	
+		urlParts.addQueries(queries)
+		urlParts.addQueries(task.queries)
+		
+		if let url = urlParts.url{
+			var request = URLRequest(url: url)
+			request.httpMethod = task.method.rawValue
+			request.addHeaders(headers)
+			request.addHeaders(task.headers)
+			
+			print(request.url!.absoluteString)
+			return request    
+		} else {
+				fatalError("urlParts did not unwrap \(urlParts)")
+		}
+	}
+}
+
+extension URLRequest{
+	internal mutating func addHeaders(_ headers: [(name: String, value: String)]){
+		for header in headers {
+			self.addValue(header.value, forHTTPHeaderField: header.name)
+		}
+	}
+}
