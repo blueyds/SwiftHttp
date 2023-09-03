74dd46172e650b78babcf1e26a59530f4f98c419
Blueyds <9055449+blueyds@users.noreply.github.com>
2023-09-03 17:16:53 -0500

    added console logging to help debug errors

--- Sources/SwiftHttp/decode.swift
+++ Sources/SwiftHttp/decode.swift
@@ -9,19 +9,27 @@ internal func decode<T: Decodable>(
 		let decoder = JSONDecoder()
 		decoder.dateDecodingStrategy = dateDecodingStrategy
 		decoder.keyDecodingStrategy = keyDecodingStrategy
-	
 	do {
 		return try decoder.decode(T.self, from: data)
 	} catch DecodingError.keyNotFound(let key, let context){
-		fatalError("Failed to decode due to missing key '\(key.stringValue)' not found 0 \(context.debugDescription)---\(context.codingPath)")
+		printKeyPath(context.codingPath)
+		fatalError("Failed to decode due to missing key '\(key.stringValue)' not found \(context.debugDescription)")
 	} catch DecodingError.typeMismatch(_, let context){
+		printKeyPath(context.codingPath)
 		fatalError("Failed to decode due to type mismatch - \(context.debugDescription)")
 	} catch DecodingError.valueNotFound(let type, let context){
+		printKeyPath(context.codingPath)
 		fatalError("Failed to decode due to missing \(type) value - \(context.debugDescription)")
 	} catch DecodingError.dataCorrupted(_){
 		fatalError("Failed to decode because it appears to be invalid JSON")
 	} catch {
 		fatalError("Failed to decode: \(error.localizedDescription)")
+	}
+}
+fileinternal func printKeyPath(_ keys: [CodingKey]){
+	print("*****KEY PATH to FAILURE******")
+	for key in keys {
+		print(key.stringValue)
 	}
 }
 
